0bba4540d02e1502d8fb452b7dd3689685343fdd
kirikonecheck <abobusiak@mail.ru>
2026-03-27 08:29:11 +0000

    im hitler

--- client.cpp
+++ client.cpp
@@ -7,41 +7,59 @@
 #include <arpa/inet.h>
 
 const char* SERVER_IP = "127.0.0.1";
-const int SERVER_PORT =  8080;
+const int SERVER_PORT = 8080;
 const int BUFFER_SIZE = 1024;
 
-int main ()
-{
-int clientSocket = socket(AF_INET, SOCK_STREAM, 0); 
-if (clientSocket < 0 )
-{
-    cerr << "" << return 1;
-}
-cout << "сокет создан";
-sockaddr_in serverAddr;
-memset (&serverAddr, 0, suzeof(serverAddr));
-serverAddr.sen_family = AF_INET;
-serverAddr.sin_port = htons (SERVER_PORT);
-inet_pton(AF_INET, SERVER_IP, &serverAddr.sin_addr);
-if (connect(clientSocket, (serverAddr*)&serverAddr, seziof(serverAddr)))
-{
-    close (clientSocket)
-    return 2;
-}
-cout << "УРА! коннект!!!";
-for (int i = 0; i <= 4; i++)
-{
-    std:string message = "ping";
-    send(clientSocket, message.c_str(), message.length(), 0);
-    char buffer [BUFFER_SIZE];
-    memset (buffer, 0, BUFFER_SIZE);
-    int bytesRecieved = recieve(clientSocket, buffer, BUFFER_SIZE-1, 0)
-    if (bytesRecieved > 0)
-    {
-        cout << buffer << endl;
+int main() {
+    
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0); 
+    if (clientSocket < 0) {
+        std::cerr << "Ошибка создания сокета" << std::endl;
+        return 1;
     }
-}
+    std::cout << "Сокет создан" << std::endl;
+
+    
+    sockaddr_in serverAddr;
+    memset(&serverAddr, 0, sizeof(serverAddr)); 
+    serverAddr.sin_family = AF_INET;           
+    serverAddr.sin_port = htons(SERVER_PORT);
+    
+    
+    if (inet_pton(AF_INET, SERVER_IP, &serverAddr.sin_addr) <= 0) {
+        std::cerr << "Неверный адрес" << std::endl;
+        close(clientSocket);
+        return 3;
+    }
+
+    
+    if (connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+        std::cerr << "Ошибка соединения" << std::endl;
+        close(clientSocket);
+        return 2;
+    }
+    std::cout << "УРА! коннект!!!" << std::endl;
+
+
+    for (int i = 0; i <= 4; i++) {
+        std::string message = "ping";
+        send(clientSocket, message.c_str(), message.length(), 0);
+        
+        char buffer[BUFFER_SIZE];
+        memset(buffer, 0, BUFFER_SIZE);
+        
+        
+        int bytesRecieved = recv(clientSocket, buffer, BUFFER_SIZE - 1, 0);
+        if (bytesRecieved > 0) {
+            std::cout << "Ответ от сервера: " << buffer << std::endl;
+        }
+        
+        sleep(1); 
+    }
+
+    
     send(clientSocket, "exit", 4, 0);
     close(clientSocket);
-    return 0;
+    
+    return 0; 
 }
--- server.cpp
+++ server.cpp
@@ -9,39 +9,82 @@
 const int SERVER_PORT = 8080;
 const int BUFFER_SIZE = 1024;
 
-int main(){
-
-
-    serverAddr.sin_addr.sin_addr = INADDR_ANY;
-    bind(socket, serverAddr, BUFFER_SIZE)
-    if(bind(server_socket, sockaddr*) & serverAddr, sizeof(serverAddr) < 0){
-        std::cerr << "Ошибка bind";
-        close(server_socket);
-        return 2; 
+int main() {
+    
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0) {
+        std::cerr << "Ошибка создания сокета" << std::endl;
+        return 1;
     }
-    if((listen(server_socket), 3) < 0){
-        stdd::cerr << "О очереди" << close(server_socket);
+
+    
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(SERVER_PORT);
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+    
+    if (bind(server_socket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+        std::cerr << "Ошибка bind" << std::endl;
+        close(server_socket);
+        return 2;
+    }
+
+    
+    if (listen(server_socket, 3) < 0) {
+        std::cerr << "Ошибка listen" << std::endl;
+        close(server_socket);
         return 3;
-    };
-    std::cout  << "Сори слушаю" << SERVER_PORT << endl;
-    std:: cout << "Ожидание клиента";
+    }
+
+    std::cout << "Сервер слушает порт: " << SERVER_PORT << std::endl;
+    std::cout << "Ожидание клиента..." << std::endl;
+
+    
     sockaddr_in clientAddr;
-    socklen_t clientAddrSize = sizeof(clientAddr),
-    int clientSocket = accept(server_socket, (sockaddr*) clientAddr, & clientAddrSize);
-    if(clientSocket < 0){
-        close(server_socket); 
+    socklen_t clientAddrSize = sizeof(clientAddr);
+    int clientSocket = accept(server_socket, (struct sockaddr*)&clientAddr, &clientAddrSize);
+    
+    if (clientSocket < 0) {
+        std::cerr << "Ошибка accept" << std::endl;
+        close(server_socket);
         return 4;
     }
 
-    char clientIP[NET_ADDRSTRLEN];
-    inet_htop(AF_INET, 8 CLIENT  2);
-    CLIENTIP, IN_LEN;
-    while(true){
-        memset(buffer, a, BUFF, ze);2
-    }
-    int bytesReceived = recv(client_socket, buffer, BUFFER_SIZE, 0);
-    if(bytesReceived <= 0) {
+    char clientIP[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &clientAddr.sin_addr, clientIP, INET_ADDRSTRLEN);
+    std::cout << "Подключен клиент: " << clientIP << std::endl;
+
+    char buffer[BUFFER_SIZE];
+
+    
+    while (true) {
+        memset(buffer, 0, BUFFER_SIZE);
         
-        break;
+        int bytesReceived = recv(clientSocket, buffer, BUFFER_SIZE - 1, 0);
+        if (bytesReceived <= 0) {
+            std::cout << "Клиент отключился" << std::endl;
+            break;
+        }
+
+        std::string message(buffer);
+        if (message == "exit") {
+            std::cout << "Выход..." << std::endl;
+            break;
+        }
+
+        std::string response;
+        if (message == "ping") {
+            response = "pong";
+        } else {
+            response = "Неизвестно";
+        }
+
+        std::cout << "Получено: " << message << " -> Ответ: " << response << std::endl;
+        send(clientSocket, response.c_str(), response.length(), 0);
     }
+
+    close(clientSocket);
+    close(server_socket);
+    return 0;
 }
